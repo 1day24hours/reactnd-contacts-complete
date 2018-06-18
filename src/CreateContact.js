4c4afefb527e5c20817ebd05e3f03caa9db8820d
Felix Chen <chen.felix.xi@gmail.com>
2018-06-18 14:45:34 +0800

    modified 'e.preventDefault()' issue

--- src/CreateContact.js
+++ src/CreateContact.js
@@ -5,7 +5,7 @@ import serializeForm from 'form-serialize'
 
 class CreateContact extends Component {
     handleSubmit = (e) => {
-        e.preventDafault()
+        e.preventDefault()
         const values = serializeForm(e.target, { hash: true })
         if (this.props.onCreateContact)
         this.props.onCreateContact(values)
